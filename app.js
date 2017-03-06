8f02447b9929b83c02387b4c3e1f6b36f266cb1c
Nicolás Gómez <ngomez@hasu.cl>
2017-03-05 21:28:58 -0300

    Added images, now home is the digimon field, changed general style, removed digivolution tiers

--- app.js
+++ app.js
@@ -1,49 +1,89 @@
 $.getJSON("data.json", function(db) {
 
-  // Database Parser
-  var evolutions = _.keys(db);
+  $.getJSON('field.json', function(field){
 
-  console.log(db);
+    // Database Parser
 
-  // Vue
+    var evolutions = _.keys(db);
+    var list = [];
 
-  var example1 = new Vue({
-    el: '#app',
-    data: {
-      items: db,
-      evolutions: Object.keys(db),
-      pickedEvolution: 'In-Training I',
-      search: '',
-      selected: null,
-      show: false
-    },
-    computed: {
-      searchResult: function () {
+    _.each(evolutions, function(evolution){
+      _.each(db[evolution], function(digimon){
+        digimon['image_url'] = 'images/' + digimon.name + '.jpg';
+        digimon['evolution_image'] = 'images/' + digimon.evolution + '.jpg';
+        digimon['tier'] = evolution;
+        list.push(digimon);  
+      })
+    })
 
-        var items = db[this.pickedEvolution]
+    // Vue
 
-        return items.filter(function (item) {
-          return item.name.toLowerCase().indexOf(this.search.toLowerCase()) >= 0
-        }.bind(this))
+    var example1 = new Vue({
+      el: '#app',
+      data: {
+        evolutions: Object.keys(db),
+        pickedEvolution: 'In-Training I',
+        search: '',
+        list: field,
+        selected: null, // Used to check evolutions
+        show: false,
+        evolution: null // Used for modal
+      },
+      computed: {
+        searchResult: function () {
+          return this.list.filter(function (digimon) {
+            return digimon.name.toLowerCase().indexOf(this.search.toLowerCase()) >= 0
+          }.bind(this))
+        },
+        modalTitle: function(){
+          if(this.selected && this.evolution){
+            return this.selected.name + ' -> ' + this.evolution.evolution + ' Requeriments';
+          }
+        },
+        selectedEvolutions: function(){
+
+          if(this.selected){
+
+            return list.filter(function(digimon){
+              return digimon.name.toLowerCase() == this.selected.name.toLowerCase()
+            }.bind(this))
+
+          } else {
+            return [];
+          }
+
+        },
+
+        evolutionAttrs: function(){
+
+          if(this.evolution){
+            return _.omit(this.evolution, ['image_url', 'tier', 'evolution_image', 'name', 'evolution'])
+          }
+
+        },
+
+        evolutionImage: function(){
+          if(this.evolution){
+            return this.evolution.evolution_image
+          }
+        }
 
       },
-      modalTitle: function(){
-        if(this.selected){
-          return this.selected.name + ' -> ' + this.selected.evolution
+      methods: {
+        showInfo: function(evolution){
+
+          this.evolution = evolution;
+
+          $('#modal').modal()
+
+        },
+
+        select: function(selected){
+          this.selected = selected;
         }
       }
-    },
-    methods: {
-      showInfo: function(selected){
+    })
 
-        this.selected = selected;
-
-        $('#modal').modal()
-
-      }
-    }
   })
-
-
 
 });
